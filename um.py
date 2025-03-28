9547da5e7572e934706e4d5ec05cd4914f2a9018
Nicole Tomazoni Pelentir <124601530+nicoletomazonipelentir@users.noreply.github.com>
2025-03-28 19:04:10 -0300

    Update um.py

--- um.py
+++ um.py
@@ -1,3 +1,1 @@
-#1) Extrator de caracteres de um arquivo - Desenvolva um programa que leia um arquivo de
-e o decomponha na sequência de caracteres ASCII
-individuais nele contidosa
+#1) Extrator de caracteres de um arquivo - Desenvolva um programa que leia um arquivo de e o decomponha na sequência de caracteres ASCII individuais nele contidosa
